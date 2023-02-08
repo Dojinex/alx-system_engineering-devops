2f6369ca3cb2d953e95f89c0acfa124c10d1ed73
Dojinex <bitrusdauda9118@gmail.com>
2023-02-08 18:03:24 +0000

    All task commit

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,14 +3,11 @@
    export information about the TODO list progress for all employees IDs
    to a file (JSON format), file name must be: todo_all_employees.json"""
 
-
 import json
 import requests
 
-
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-
 
     user_dict = {}
     name_dict = {}
@@ -20,7 +17,6 @@ if __name__ == '__main__':
         user_dict[user_id] = []
         name_dict[user_id] = user.get("username")
 
-
     todo = requests.get(url + "todos").json()
     for task in todo:
         user_id = task.get("userId")
@@ -29,7 +25,6 @@ if __name__ == '__main__':
         task_dict["completed"] = task.get("completed")
         task_dict["username"] = name_dict.get(user_id)
         user_dict.get(user_id).append(task_dict)
-
 
     with open("todo_all_employees.json", "w") as file:
         json.dump(user_dict, file)
